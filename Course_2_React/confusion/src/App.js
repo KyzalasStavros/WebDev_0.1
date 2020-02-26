f8ea7ed1c4da0f8c716f841460d63e2a01c869e2
Kyzalas Stavros <KyzalasStavros@KyzalasStavros.local>
2020-02-26 13:18:24 +0200

    react comp part 2. so ugly

--- Course_2_React/confusion/src/App.js
+++ Course_2_React/confusion/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 
 import './App.css';
+import {DISHES} from './shared/dishes';
 
-function App() {
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state={
+      dishes:DISHES
+    };
+  }
+
+  render(){
   return (
     <div>
       <Navbar dark color="primary">
@@ -14,7 +25,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
 
 
       {/* 
@@ -39,4 +50,5 @@ function App() {
   );
 }
 
+}
 export default App;
